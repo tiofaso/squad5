fc8ff05b3c85b65b791a7340148e97084526c1f1
Thaís Breder <thais.breder@zup.com.br>
2023-10-03 18:48:57 -0300

    ID 50: Exceção para ID da falha não encontrado

---
+++ src/main/java/com/catalisa/squad5/exceptions/IssueIdNotFound.java
@@ -0,0 +1,7 @@
+package com.catalisa.squad5.exceptions;
+
+public class IssueIdNotFound extends RuntimeException {
+    public IssueIdNotFound(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/catalisa/squad5/exceptions/IssueNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.catalisa.squad5.exceptions;
-
-public class IssueNotFound extends RuntimeException {
-    public IssueNotFound(String message) {
-        super(message);
-    }
-}
