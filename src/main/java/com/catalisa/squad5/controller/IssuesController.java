62ec70277edc64f1260545ee9c9f0f0888202503
Thaís Breder <thais.breder@zup.com.br>
2023-10-03 18:46:31 -0300

    ID 50: Métodos para salvar/registrar, consultar por ID e listar falhas de acessibilidade

--- src/main/java/com/catalisa/squad5/controller/IssuesController.java
+++ src/main/java/com/catalisa/squad5/controller/IssuesController.java
@@ -1,7 +1,38 @@
 package com.catalisa.squad5.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.catalisa.squad5.model.Issues;
+import com.catalisa.squad5.service.IssuesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/issues")
 public class IssuesController {
+
+    @Autowired
+    IssuesService issuesService;
+
+    //Registra nova falha de acessibilidade
+    @PostMapping
+    public ResponseEntity<Issues> registerIssue(@RequestBody Issues issues) {
+        Issues newIssue = issuesService.registerIssue(issues);
+        return new ResponseEntity<>(issues, HttpStatus.CREATED);
+    }
+
+    //Listar todas as falhas
+    @GetMapping
+    public List<Issues> findAll() {
+        return issuesService.findAll();
+    }
+
+    //Buscar falha por id
+    @GetMapping(path = "{id}")
+    public ResponseEntity<Issues> getByIdId(@PathVariable Long id) {
+        Issues issue = issuesService.getById(id);
+        return ResponseEntity.ok(issue);
+    }
 }
--- src/main/java/com/catalisa/squad5/service/IssuesService.java
+++ src/main/java/com/catalisa/squad5/service/IssuesService.java
@@ -1,7 +1,28 @@
 package com.catalisa.squad5.service;
 
+import com.catalisa.squad5.exceptions.IssueNotFound;
+import com.catalisa.squad5.model.Issues;
+import com.catalisa.squad5.repository.IssuesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class IssuesService {
+    @Autowired
+    IssuesRepository issuesRepository;
+
+    public Issues registerIssue(Issues updateIssues) {
+        return issuesRepository.save(updateIssues);
+    }
+
+    public List<Issues> findAll() {
+        return issuesRepository.findAll();
+    }
+
+    public Issues getById(Long id) {
+        return issuesRepository.findById(id)
+                .orElseThrow(() -> new IssueNotFound("Issue with ID: " + id + " not found."));
+    }
 }
