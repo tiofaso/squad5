de41c099e6e255f32d6dd7ce8d1bdad207ed611b
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-10-05 17:46:18 -0300

    :zap: Adição e remoção de beans de validação

--- src/main/java/com/catalisa/squad5/model/Issues.java
+++ src/main/java/com/catalisa/squad5/model/Issues.java
@@ -3,8 +3,11 @@ package com.catalisa.squad5.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -19,12 +22,17 @@ public class Issues {
     private Long id;
 
     @Column(name = "url", length = 255)
+    @NotBlank(message = "A URL não pode estar em branco")
+    @NotNull(message = "A URL não pode estar vazia")
+    @URL(message = "URL inválida")
     private String url;
 
     @Column(name = "name_company", length = 255)
+    @NotBlank(message = "O nome da empresa não pode estar em branco")
     private String nameCompany;
 
     @Column(name = "description", columnDefinition = "text")
+    @NotBlank(message = "A descrição da falha de acessibilidade não pode estar em branco")
     private String description;
 
     @Column(name = "task", length = 1)
@@ -35,9 +43,9 @@ public class Issues {
 
     @Column(name = "time")
     private LocalTime time;
+
     @ManyToOne
     @JoinColumn(name = "manager")
     private Users manager;
 }
-
 
