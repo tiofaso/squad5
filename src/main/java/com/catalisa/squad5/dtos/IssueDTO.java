17d9567d3e6bac493a7c7e4e6bf37682a0f0af31
Thaís Breder <thais.breder@zup.com.br>
2023-10-05 11:23:10 -0300

    ID 89: Tentativa de implementação de validação

--- src/main/java/com/catalisa/squad5/dtos/IssueDTO.java
+++ src/main/java/com/catalisa/squad5/dtos/IssueDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -11,10 +13,18 @@ import java.time.LocalTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class IssueDTO {
-
+    @NotBlank(message = "A URL não pode estar em branco")
     private String url;
+
+    @NotBlank(message = "O nome da empresa não pode estar em branco")
     private String nameCompany;
+
+    @NotBlank(message = "A descrição da falha de acessibilidade não pode estar em branco")
     private String description;
+
+    @NotNull(message = "A data não pode ser nula")
     private LocalDate date;
+
+    @NotNull(message = "O horário não pode ser nulo")
     private LocalTime time;
 }
--- src/main/java/com/catalisa/squad5/model/Issues.java
+++ src/main/java/com/catalisa/squad5/model/Issues.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -19,24 +22,32 @@ public class Issues {
     private Long id;
 
     @Column(name = "url", length = 255)
+    @NotBlank(message = "A URL não pode estar em branco")
     private String url;
 
     @Column(name = "name_company", length = 255)
+    @NotBlank(message = "O nome da empresa não pode estar em branco")
     private String nameCompany;
 
     @Column(name = "description", columnDefinition = "text")
+    @NotBlank(message = "A descrição da falha de acessibilidade não pode estar em branco")
     private String description;
 
     @Column(name = "task", length = 1)
+    @NotBlank(message = "A tarefa não pode estar em branco")
     private String task;
 
     @Column(name = "date")
+    @NotNull(message = "A data não pode ser nula")
     private LocalDate date;
 
     @Column(name = "time")
+    @NotNull(message = "O horário não pode ser nulo")
     private LocalTime time;
+
     @ManyToOne
     @JoinColumn(name = "manager")
+    @NotNull(message = "O responsável não pode ser nulo")
     private Users manager;
 }
 
