271d4f7f86b7d44b954b03b91747b53704deef11
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-10-10 15:38:37 -0300

    :bug: ID 108 - Toda segurança da API foi desabilitada até implementação do JWT

--- src/main/java/com/catalisa/squad5/front/js/formulario.js
+++ src/main/java/com/catalisa/squad5/front/js/formulario.js
@@ -1,59 +1,60 @@
-const username = 'admin';
-const password = '12345';
-const base64Credentials = btoa(`${username}:${password}`);
+function reportIssue(event){
+  event.preventDefault();
 
-const formData = new FormData(document.getElementById('issue-form')); // Obtém os dados do formulário
+  //This is a temporary code to lock the session active
+  const username = 'admin';
+  const password = '12345';
+  const credentials = `${username}:${password}`;
+  const base64Credentials = btoa(credentials);
 
-fetch('http://localhost:8080/issues', {
-    method: 'GET',
-    body: formData,
-    headers: {
-        'Authorization': `Basic ${base64Credentials}`
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://localhost:8080/issues', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('Authorization', `Basic ${base64Credentials}`);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+            //------
+
+            //Everything is fine and logged. Now do the magic!!!
+            const url = document.getElementById('endereco-site').value;
+            const nameCompany = document.getElementById('nome-empresa').value;
+            const email_reporter = document.getElementById('email').value;
+            const description = document.getElementById('descricao-problema').value;
+
+            //Building the payload
+            const payload = JSON.stringify({
+                url,
+                nameCompany,
+                email_reporter,
+                description
+            });
+
+            //Configuring fetch
+            const requestOptions = {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: payload
+            };
+            
+            //Send payload through endpoint
+            fetch('http://localhost:8080/issues', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+              console.log('Issue reported successfully:', data);
+              // Add any further actions you want to take after reporting the issue
+            })
+            .catch(error => console.error('Error reporting the issue:', error));
+            
+            //------
+        } else if (xhr.status === 401) {
+            document.getElementById('msg').style.display = 'block';
+        }
     }
-})
-.then(response => {
-    if (response.status === 201) {
+};
 
-        //Cadastro de alunos
-function cadastraAluno(event) {
-    event.preventDefault();
-
-    const endereco = document.getElementById('endereco-site').value;
-    const empresa = document.getElementById('nome-empresa').value;
-    const email = document.getElementById('email').value;
-    const descricao = document.getElementById('descricao-problema').value;
-
-    // Montando json
-    const payload = JSON.stringify({
-      endereco,
-      empresa,
-      email,
-      descricao
-    });
-
-    fetch('http://localhost:8080/issues', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: payload
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-        // Display the success message
-        document.getElementById('msgsucesso').style.display = 'block';
-        document.getElementById('cadastrofalha').reset();  // Reset the form
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+xhr.send();
 }
-    } else {
-        // Tratamento para erro
-        alert('Erro ao registrar. Por favor, verifique os campos e tente novamente.');
-    }
-})
-.catch(error => {
-    console.error('Erro ao enviar os dados:', error);
-});
