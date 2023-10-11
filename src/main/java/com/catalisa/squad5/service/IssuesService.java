c6a854ba8c0d1470145b4e4b2896c68de2200c9b
Thaís Breder <thais.breder@zup.com.br>
2023-10-11 15:20:54 -0300

    Construção do método PUT para a atualizar a variável task

--- src/main/java/com/catalisa/squad5/service/IssuesService.java
+++ src/main/java/com/catalisa/squad5/service/IssuesService.java
@@ -1,5 +1,6 @@
 package com.catalisa.squad5.service;
 
+
 import com.catalisa.squad5.dtos.IssuesDTO;
 import com.catalisa.squad5.exceptions.IssueIdNotFound;
 import com.catalisa.squad5.model.Issues;
@@ -7,10 +8,13 @@ import com.catalisa.squad5.repository.IssuesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class IssuesService {
@@ -44,6 +48,15 @@ public class IssuesService {
         existingIssue.setDescription(issuesDTO.getDescriptionDto());
         existingIssue.setDate(issuesDTO.getDateDto());
         existingIssue.setTime(issuesDTO.getTimeDto());
+
+        return issuesRepository.save(existingIssue);
+    }
+
+    public Issues updateTask(Long id, Integer newTask) {
+        Optional<Issues> optionalIssue = issuesRepository.findById(id);
+        Issues existingIssue = optionalIssue.orElseThrow(() -> new IssueIdNotFound("Issue with ID: " + id + " not found."));
+
+        existingIssue.setTask(newTask);
 
         return issuesRepository.save(existingIssue);
     }
