ab006085bdda04f90ea707b485504708b2f6df5e
Thaís Breder <thais.breder@zup.com.br>
2023-10-11 15:52:07 -0300

    Construção da função para o envio da requisição GET e para popular o kanban(dashboard)

--- src/main/java/com/catalisa/squad5/front/js/kanban.js
+++ src/main/java/com/catalisa/squad5/front/js/kanban.js
@@ -2,63 +2,85 @@ const username = 'admin';
 const password = '12345';
 const credentials = `${username}:${password}`;
 const base64Credentials = btoa(credentials);
+let issues = []; // Array para armazenar os cards
 
+
+// Configuração da requisição AJAX
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://localhost:8080/issues', true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.setRequestHeader('Authorization', `Basic ${base64Credentials}`);
 
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            const allIssues = JSON.parse(xhr.responseText);
 
-            populateKanban(allIssues);
-        } else if (xhr.status === 401) {
-            document.getElementById('msg').style.display = 'block';
-        }
-    }
+xhr.onreadystatechange = function() {
+// Função que é chamada sempre que o estado da requisição muda
+if (xhr.readyState === XMLHttpRequest.DONE) {
+if (xhr.status === 200) {
+// Se a requisição foi bem-sucedida, atualiza os cards
+issues = JSON.parse(xhr.responseText);
+populateKanban(issues);
+} else if (xhr.status === 401) {
+// Se houve erro de autorização, exibe uma mensagem
+document.getElementById('msg').style.display = 'block';
+}
+}
 };
 
-xhr.send();
 
+xhr.send(); // Envio da requisição GET
+
+
+
+
+// Função para popular o kanban com os cards
 function populateKanban(issues) {
-    const issuesToDo = document.getElementById("issuesToDo");
-    const issuesDoing = document.getElementById("issuesDoing");
-    const issuesDone = document.getElementById("issuesDone");
+const issuesToDo = document.getElementById("issuesToDo");
+const issuesDoing = document.getElementById("issuesDoing");
+const issuesDone = document.getElementById("issuesDone");
 
-    // Limpa o conteúdo antes de adicionar os cards
-    issuesToDo.innerHTML = "";
-    issuesDoing.innerHTML = "";
-    issuesDone.innerHTML = "";
 
-    let todoCount = 0;
-    let doingCount = 0;
-    let doneCount = 0;
+// Limpa o conteúdo antes de adicionar os cards
+issuesToDo.innerHTML = "";
+issuesDoing.innerHTML = "";
+issuesDone.innerHTML = "";
 
-    issues.forEach(values => {
-        const issueElement = `
+
+let todoCount = 0;
+let doingCount = 0;
+let doneCount = 0;
+
+
+// Itera sobre os cards e os adiciona ao kanban conforme a coluna
+issues.forEach(values => {
+// Gera a estrutura HTML para cada card
+const issueElement = `
 <li class="list-group-item">
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">ID: ${values.id}</h5>
-            <p class="card-text"><strong>URL:</strong> ${values.url}</p>
-            <p class="card-text"><strong>Empresa:</strong> ${values.nameCompany}</p>
-            <p class="card-text"><strong>Descrição:</strong> ${values.description}</p>
-        </div>
-    </div>
+<div class="card">
+<div class="card-body">
+<h5 class="card-title">ID: ${values.id}</h5>
+<p class="card-text"><strong>URL:</strong> ${values.url}</p>
+<p class="card-text"><strong>Empresa:</strong> ${values.nameCompany}</p>
+<p class="card-text"><strong>Descrição:</strong> ${values.description}</p>
+<div class="btn-group" role="group">
+<button class="btn btn-secondary" onclick="moveToInProgress(${values.id})">In Progress</button>
+<button class="btn btn-secondary" onclick="moveToDone(${values.id})">Done</button>
+</div>
+</div>
+</div>
 </li>
 `;
 
-        if (values.task === 0 && todoCount < 3) {
-            issuesToDo.innerHTML += issueElement;
-            todoCount++;
-        } else if (values.task === 1 && doingCount < 3) {
-            issuesDoing.innerHTML += issueElement;
-            doingCount++;
-        } else if (values.task === 2 && doneCount < 3) {
-            issuesDone.innerHTML += issueElement;
-            doneCount++;
-        }
-    });
+
+// Adiciona o card à coluna correspondente
+if (values.task === 0 && todoCount < 3) {
+issuesToDo.innerHTML += issueElement;
+todoCount++;
+} else if (values.task === 1 && doingCount < 3) {
+issuesDoing.innerHTML += issueElement;
+doingCount++;
+} else if (values.task === 2 && doneCount < 3) {
+issuesDone.innerHTML += issueElement;
+doneCount++;
+}
+});
 }
