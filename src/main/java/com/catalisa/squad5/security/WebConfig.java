c143fca0960d499b6df7b81a45027f77328f1284
Thaís Breder <thais.breder@zup.com.br>
2023-10-13 11:52:35 -0300

    Altera url para permitir acesso ao endpoint /issues/id

--- src/main/java/com/catalisa/squad5/security/WebConfig.java
+++ src/main/java/com/catalisa/squad5/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/issues")
+        registry.addMapping("/issues**")
                 .allowedOrigins("http://127.0.0.1:5500");
     }
 }
